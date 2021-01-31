6427255a129ca0b08df329ebc364b3b3d8c53968
liangzisong <18462146261@163.com>
2021-01-31 17:26:33 +0800

    first commit

---
+++ src/main/java/com/liangzisong/reptileyymh/ReptileYymhApplication.java
@@ -0,0 +1,13 @@
+package com.liangzisong.reptileyymh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptileYymhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReptileYymhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liangzisong/reptileyymh/ReptileYymhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangzisong.reptileyymh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReptileYymhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
